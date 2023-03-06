59707dd06369d0ac9d57bc9300592b5c593b9d06
daniell-olaitan <daniell.olaitan@gmail.com>
2023-03-06 20:00:01 +0100

    attempt

--- monty.h
+++ monty.h
@@ -62,5 +62,6 @@ void free_stack(stack_t *stack);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 
 #endif
